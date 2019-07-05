be1cd2bb91a7ceddce27cd437de116247f22db89
David O'Dell <34406740+texasdave2@users.noreply.github.com>
2019-07-05 13:35:17 -0500

    Update license-days-remaining.sh

--- textfile-collector/parse-configmap/license-days-remaining.sh
+++ textfile-collector/parse-configmap/license-days-remaining.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-## script to calculate days remaining of an application license inside a kubernetes config map
+## script to calculate days remaining in licenses from kubernetes config map
 ## and format them into prometheus-friendly metrics to be used by
 ## node exporter textfile collector
-
 
 license_name_array=( $(sudo kubectl describe cm license-config | \
 awk '/licenceInfo/ {print $3;}' | \
@@ -15,12 +14,20 @@ cut -d '"' -f2) )
 
 for i in "${license_name_array[@]}"
 do
+
+  clean_name=$(echo $i | sed -e 's/-//g')
+
   for j in "${license_expire_date_array[@]}"
   do
         days_remain=$( echo $(( ($(date -d $j +%s) - $(date +%s) )/(60*60*24) )) )
         expire_date=$j
   done
 
-  echo "license_days_remaining_$i $days_remain"
+  echo "license_days_remaining_$clean_name $days_remain"
 
-done
+done > /var/lib/node_exporter/license_days.prom
+
+chown cloud-user:cloud-user /var/lib/node_exporter/license_days.prom &&
+chmod 744 /var/lib/node_exporter/license_days.prom
+
+
